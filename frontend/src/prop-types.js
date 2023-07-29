c13eb740e2aa8f0032f9a3818fab75f9f35b1c93
blas <blashernandez98@gmail.com>
2023-07-29 00:26:52 -0300

    New code

--- frontend/src/prop-types.js
+++
@@ -1,9 +0,0 @@
-
-import PropTypes from 'prop-types';
-
-export const notePropTypes = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
-  updated_at: PropTypes.string.isRequired,
-});
